988a276956bda3aa9e5bcc422ba8594d531c22e4
1125bid <dlfdlfdldh1125@naver.com>
2024-12-08 09:44:58 +0900

    feat: textfield추가

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,6 +3,30 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final controller = TextEditingController();
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+          appBar: AppBar(
+            title: SizedBox(
+              height: 50,
+              child: Expanded(
+                child: TextField(
+                  controller: controller,
+                  decoration: InputDecoration(
+                      hintText: '지역을 입력해주세요.',
+                      border: MaterialStateOutlineInputBorder.resolveWith(
+                        (states) {
+                          return const OutlineInputBorder();
+                        },
+                      )),
+                ),
+              ),
+            ),
+          ),
+          body: ListView()),
+    );
   }
 }
---
+++ lib/widget/appbar_textfield.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppbarTextfield extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextField();
+  }
+}
